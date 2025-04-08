db250fc758b41b3354af38bac84b8b25acbaae5b
ReaperXYZ <166727639+ReaperXYZ@users.noreply.github.com>
2025-04-07 23:39:19 -0400

    Created deletion and update functions for the database

--- moves/SQL/Login.py
+++ moves/SQL/Login.py
@@ -9,7 +9,7 @@ def getDatabaseForAccountTable(usernameinput, passwordinput):
             user=usernameinput,
             password=passwordinput,
             host='127.0.0.1',
-            port=3307,
+            port=3306,
             database='movesprofile'
         )
         print("got cursor")
@@ -25,8 +25,11 @@ def getDatabaseForAccountTable(usernameinput, passwordinput):
 
 def testLogin(database, email, password):
     cursor = database.cursor()
+    if not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+        print("Invalid email")
+        return database, False
     cursor.execute(
-        'SELECT * FROM account WHERE email = %s AND password = %s',
+        'SELECT * FROM account WHERE email = %s AND password = SHA2(%s, 512)',
         (email, password,)
     )
     account = cursor.fetchone()
@@ -36,19 +39,96 @@ def testLogin(database, email, password):
 
 def createAccount(database, username, email, password):
     cursor = database.cursor()
+    if not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+        print("Invalid email")
+        return database, False
     cursor.execute(
             'SELECT * FROM account WHERE email = %s', (email, ))
     account = cursor.fetchone()
     if account:
         print("Account already exists!")
         return database, False
-    elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-        print("Invalid email")
-        return database, False
     else:
         cursor.execute('INSERT INTO account'
                        '(username, email, password)'
-                       'VALUES (%s, %s, AES_ENCRYPT(%s, "temp"))',
+                       'VALUES (%s, %s, SHA2(%s, 512))',
                        (username, email, password, ))
+        database.commit()
+        return database, True
+
+def removeAccount(database, email):
+    cursor = database.cursor();
+    if not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+        print("Invalid email")
+        return database, False
+    cursor.execute(
+            'SELECT * FROM account WHERE email = %s', (email, ))
+    account = cursor.fetchone()
+    if not account:
+        print("Account does not exist!")
+        return database, False
+    else:
+        cursor.execute('DELETE FROM account where email = %s',
+                       (email, ))
+        database.commit()
+        return database, True
+
+def updateEmail(database, email, newemail):
+    cursor = database.cursor();
+    if not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+        print("Invalid email")
+        return database, False
+    if not re.match(r'[^@]+@[^@]+\.[^@]+', newemail):
+        print("Invalid email")
+        return database, False
+    cursor.execute(
+            'SELECT * FROM account WHERE email = %s', (email, ))
+    account = cursor.fetchone()
+    if not account:
+        print("Account does not exist!")
+        return database, False
+    cursor.execute(
+            'SELECT * FROM account WHERE email = %s', (newemail, ))
+    account = cursor.fetchone()
+    if account:
+        print("Account already exists!")
+        return database, False
+    else:
+        cursor.execute('UPDATE account SET email = %s WHERE email = %s',
+                       (newemail, email, ))
+        database.commit()
+        return database, True
+
+def updateUsername(database, email, username):
+    cursor = database.cursor();
+    if not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+        print("Invalid email")
+        return database, False
+    cursor.execute(
+            'SELECT * FROM account WHERE email = %s', (email, ))
+    account = cursor.fetchone()
+    if not account:
+        print("Account does not exist!")
+        return database, False
+    else:
+        cursor.execute('UPDATE account SET username = %s WHERE email = %s',
+                       (username, email, ))
+        database.commit()
+        return database, True
+
+def updatePassword(database, email, password):
+    cursor = database.cursor();
+    if not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+        print("Invalid email")
+        return database, False
+    cursor.execute(
+            'SELECT * FROM account WHERE email = %s', (email, ))
+    account = cursor.fetchone()
+    if not account:
+        print("Account does not exist!")
+        return database, False
+    else:
+        cursor.execute('UPDATE account SET password = SHA2(%s, 512) WHERE email = %s',
+                       (password, email, ))
         database.commit()
         return database, True
